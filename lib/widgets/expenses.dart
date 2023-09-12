d9fe4c1bcc753aa34526bddad97a98fedeb83d7c
seadynamic8 <seadynamic8@zoho.com>
2023-09-12 13:34:49 -0400

    Add remove expense with a dissmissble widget

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -39,6 +39,12 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,7 +61,10 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text('Chart...'),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses),
+            child: ExpensesList(
+              expenses: _registeredExpenses,
+              onRemoveExpense: _removeExpense,
+            ),
           ),
         ],
       ),
--- lib/widgets/expenses/expenses_list.dart
+++ lib/widgets/expenses/expenses_list.dart
@@ -4,15 +4,33 @@ import 'package:expense_tracker2/models/expense.dart';
 import 'package:expense_tracker2/widgets/expenses/expense_item.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList({
+    super.key,
+    required this.expenses,
+    required this.onRemoveExpense,
+  });
 
   final List<Expense> expenses;
+  final void Function(Expense) onRemoveExpense;
+
+  void removeExpense(Expense expense) {
+    onRemoveExpense(expense);
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => ExpenseItem(expenses[index]),
+      itemBuilder: (ctx, index) => Dismissible(
+        background: Container(
+          decoration: const BoxDecoration(
+            color: Colors.purple,
+          ),
+        ),
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) => removeExpense(expenses[index]),
+        child: ExpenseItem(expenses[index]),
+      ),
     );
   }
 }
