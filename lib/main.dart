3c02a6839f54fd152a90699ecfbb0917f78999e0
seadynamic8 <seadynamic8@zoho.com>
2023-09-12 14:43:25 -0400

    Add theme colors

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,37 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker2/widgets/expenses.dart';
 
+final kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData().copyWith(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          foregroundColor: kColorScheme.primaryContainer,
+          backgroundColor: kColorScheme.onPrimaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 16,
+              ),
+            ),
+      ),
       home: const Expenses(),
     ),
   );
